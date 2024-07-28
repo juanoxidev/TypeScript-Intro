a52c3396a11f262e3335c0724908e7542a4459f8
juanoxi <jm.oxinalde@hotmail.com>
2024-07-28 16:40:34 -0300

    Se implementan metodos asincronos, axios, destructuracion de la resp de la api.

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export class Pokemon {
   
 
@@ -17,9 +19,48 @@ export class Pokemon {
   readonly hace que no se puedan cambiar los atributos de esa clase. Es importante que el id no se pueda cambiar.
   */
   
-  constructor(public readonly id: number, public name: string) {}
+  constructor(public readonly id: number, public name: string) { }
   
+  // getters
 
+// uso de palabra reservada get paraindicar que es un getter
+  get imageUrl(): string {
+    return `https://pokemon.com/${this.id}.jpg`;
+  }
+  
+  // metodos de la clase por defecto todo lo q definimos en la clase es publico
+ public scream(): void  {
+    console.log(`${this.name.toUpperCase()}!!!`)
+  }
+
+  // se puede o no indicar que es void
+public speak(): void {
+  console.log(this.speakContent())
+  }
+
+  // solo se accede desde la clase.
+  private speakContent(): string {
+    return `${this.name}, ${this.name}`
+  }
+    
+    // metodo asincrono que regresa una promesa: Es devolver lo que estamos indicando que va a devolver. Tiene un estado y un resultado.
+  
+  async getMoves() {
+    // dentro de una funcion o metodo asincrono, espera a que esta promesa se resuelva y lo que sea que reseuelva lo asigna a la resp.
+     // desestructuracion de la respuesta de la api, saca la data de la resp.
+    const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+
+    console.log(data.moves);
+    return data.moves;
+  }
+    
 }
 
+//construccion de un objeto de clase Pokemon
 export const charmander = new Pokemon(4, 'Charmander')
+// como se accede al getter
+console.log(charmander.imageUrl)
+// uso del metodo
+charmander.speak()
+charmander.scream()
+charmander.getMoves()
